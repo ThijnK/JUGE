c764ac8c01da98a13c6239b848e0170d29206c42
Thijn <thijnkroon@gmail.com>
2025-05-06 13:16:08 +0200

    Fix taresults script for large inputs

--- infrastructure/scripts/taresults.sh
+++ infrastructure/scripts/taresults.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-# author: Urko Rueda (2016)
+# author: Thijn Kroon (2025)
 
-find . -type f \( -name \*\transcript\.csv \) | xargs tar -zcvf transcripts.tar.gz
+find . -type f \( -name \*\transcript\.csv \) > transcript_files.txt
+tar -zcvf transcripts.tar.gz -T transcript_files.txt
+rm transcript_files.txt
