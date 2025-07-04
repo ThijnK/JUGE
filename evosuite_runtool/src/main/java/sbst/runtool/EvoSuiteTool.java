9f41620840f2afe937673d5aa89ebb06da325e83
Thijn <thijnkroon@gmail.com>
2025-07-04 17:38:54 +0200

    Update evosuite version and config

--- evosuite_runtool/src/main/java/sbst/runtool/EvoSuiteTool.java
+++ evosuite_runtool/src/main/java/sbst/runtool/EvoSuiteTool.java
@@ -22,7 +22,7 @@ public class EvoSuiteTool implements ITestingTool {
      */
     @Override
     public List<File> getExtraClassPath() {
-        List<File> files = new ArrayList();
+        List<File> files = new ArrayList<File>();
         File evo = new File("lib", "evosuite.jar");
         if (!evo.exists()) {
             System.err.println("Wrong EvoSuite jar setting, jar is not at: " + evo.getAbsolutePath());
@@ -56,67 +56,57 @@ public class EvoSuiteTool implements ITestingTool {
      * Run the test tool, and let it generate test cases for a given class
      *
      * @param cName      Name of the class for which unit tests should be generated
-     * @param timeBudget How long the tool must run to test the class (in
-     *                   miliseconds)
+     * @param timeBudget How long the tool must run to test the class (in seconds
      */
     @Override
     public void run(String cName, long timeBudget) {
-        long halfTime = timeBudget / 2L;
-        System.err.println("TimeBudget: " + timeBudget);
-        long initialization = halfTime / 6L;
-        long minimization = (long) (1.4D * (double) (halfTime / 6L));
-        long assertions = (long) (0.8D * (double) (halfTime / 6L));
-        long junit = halfTime / 6L;
-        long write = (long) (0.8D * (double) (halfTime / 6L));
-        if (halfTime > 720L) {
-            initialization = 120L;
-            minimization = 120L;
-            assertions = 120L;
-            junit = 120L;
-            write = 120L;
-        } else if (halfTime > 360L) {
-            initialization = 60L;
-            minimization = 60L;
-            assertions = 60L;
-            junit = 60L;
-            write = 60L;
-        }
-        long search = timeBudget - (initialization + minimization + assertions + junit + write);
+        // Assign 50% of time budget to search phase
+        long halfTimeBudget = timeBudget / 2L;
+        long searchBudget = halfTimeBudget;
+        if (timeBudget % 2L != 0)
+            searchBudget++; // If uneven time budget, favor search phase
 
-        System.err.println("Search    : " + search);
-        System.err.println("Init      : " + initialization);
-        System.err.println("Min       : " + minimization);
-        System.err.println("Ass       : " + assertions);
-        System.err.println("Extra     : " + timeBudget);
-        System.err.println("JUnit     : " + junit);
-        System.err.println("Write     : " + write);
-        List<String> commands = new ArrayList();
+        // The rest of the time budget is divided equally between assertion generation,
+        // initialization, and compilation/flakiness checks
+        // Minimization is turned off, so it does not get any time budget
+        long initBudget = halfTimeBudget / 3L;
+        long assertionBudget = halfTimeBudget / 3L;
+        long checksBudget = halfTimeBudget / 3L;
+        // Distribute the remainder, prioritizing assertion, then init
+        long remainder = halfTimeBudget % 3L;
+        if (remainder > 0)
+            assertionBudget++;
+        if (remainder > 1)
+            initBudget++;
+
+        System.err.println("Time Budget: " + timeBudget);
+        System.err.println("  Search: " + searchBudget);
+        System.err.println("  Initialization: " + initBudget);
+        System.err.println("  Assertions: " + assertionBudget);
+        System.err.println("  Checks: " + checksBudget);
+        List<String> commands = new ArrayList<String>();
         commands.addAll(Arrays.asList(
                 "-projectCP=" + this.cutClassPath,
                 "-class", cName,
                 "-Dshow_progress=false",
-                "-Dstopping_condition=MaxTime",
+                "-Dstopping_condition=MAXTIME",
                 "-Dassertion_strategy=all",
                 "-Dtest_comments=false",
-                "-Dminimize=true",
+                "-Dminimize=false",
                 "-Dinline=false",
                 "-Dcoverage=false",
                 "-Dvariable_pool=true",
-                "-Dsearch_budget=" + search,
-                "-Dglobal_timeout=" + search,
                 "-Dnew_statistics=false",
                 "-Dstatistics_backend=NONE",
-                "-Dminimization_timeout=" + minimization,
-                "-Dassertion_timeout=" + assertions,
-                "-Dinitialization_timeout=" + initialization,
-                "-Djunit_check_timeout=" + junit,
-                "-Dextra_timeout=" + (timeBudget + (long) ((int) Math.floor((double) timeBudget / 11.0D))),
-                "-Dwrite_junit_timeout=" + write,
+                "-Dsearch_budget=" + searchBudget,
+                "-Dinitialization_timeout=" + initBudget,
+                "-Dassertion_timeout=" + assertionBudget,
+                "-Djunit_check_timeout=" + checksBudget,
+                // Write timeout is the total time budget to adhere to external
+                // time budget imposed by JUGE (but usually this is really fast,
+                // so does not matter much)
+                "-Dwrite_junit_timeout=" + timeBudget,
                 "-Dtest_dir=temp/testcases",
-                "-Dp_functional_mocking=0.8",
-                "-Dfunctional_mocking_percent=0.5",
-                "-Dp_reflection_on_private=0.5",
-                "-Dreflection_start_percent=0.8",
                 "-Dreuse_leftover_time=true"));
         String[] command = new String[commands.size()];
         commands.toArray(command);
