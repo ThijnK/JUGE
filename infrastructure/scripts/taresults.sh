f170c7ce2ffc302651d772a830b9d094ab9b53ce
Thijn <thijnkroon@gmail.com>
2025-07-15 16:39:10 +0200

    Preserve only metrics/transcript.csv in archives

--- infrastructure/scripts/taresults.sh
+++ infrastructure/scripts/taresults.sh
@@ -2,6 +2,6 @@
 
 # author: Thijn Kroon (2025)
 
-find . -type f \( -name \*\transcript\.csv \) > transcript_files.txt
+find . -type f -path '*/metrics/transcript.csv' > transcript_files.txt
 tar -zcvf transcripts.tar.gz -T transcript_files.txt
 rm transcript_files.txt
